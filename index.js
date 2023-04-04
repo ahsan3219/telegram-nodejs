9edca98de68cb70f2ea5f687af665af460ad8465
Ahsan Raza <76880965+ahsan3219@users.noreply.github.com>
2023-04-04 23:00:15 +0000

    abc

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 //  This code is a chatbot that uses the OpenAI API to generate responses.
 // When receiving a '/start' message, the bot activates and sends a message with this text,
 // and then when other messages are received, it uses the OpenAI API to generate a response.
@@ -18,8 +20,20 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const myText = "Hello, tell us about your main features in 5 points.\n" +
-    "Answer my questions like a pro.\n" ;
+
+
+
+
+
+
+
+
+
+
+
+
+const myText = "" +
+    "Who are yu" ;
 
 (async () => {
     try {
@@ -31,13 +45,110 @@ const myText = "Hello, tell us about your main features in 5 points.\n" +
             // // Process the "/start" command and send brief information to the chat
             if (text === '/start') {
                 myMessage = myText;
-                await bot.sendMessage(chatId, `This chat-bot is a bot that uses the OpenAI API to create responses. 
-Upon receipt of the '/start' message, the bot is activated and sends  a message with the given text, and then upon receipt of other messages - uses the OpenAI API to generate the response. You can communicate with the bot in almost any widely used language. GPT-3:`)
-            } else { myMessage = text }
-            // Pass model settings to OpenAI
+                await bot.sendMessage(chatId, `Hi Alfredo Mensi. I am your GoogleSheet Assistant.How can I help You.Start every message with keyword "Start".For example:Assistant tell me who is adam?`)
+            } 
+
+
+            else  if (text.slice(0, 5)==="start"||text.slice(0, 5)==="Start" )  {
+              console.log("text",typeof(text));
+
+                const api_url = 
+                        `https://sheetdb.io/api/v1/keqbu2v4inoqz/ `;
+                    
+                  async function getapi(url) {
+                      
+                     let response = await fetch(url);
+                      
+                      var data = await response.json();
+         console.log("data",data);
+                      texts(data)
+                    }
+          
+                    async function texts(data){
+                        let datas=JSON.stringify(data);
+                        let anc=`The is the list of all members in json form ${datas} Kindly read it carefully and  answer the question accordingly answer should be in text form should not contain json format.Remember last question and if need answer next question accordingly.`
+                        myMessage = anc+text;
+            console.log("Mymessage ", myMessage);           
+            // let result = await textGeneration(anc);
             const completion = await openai.createCompletion({
                 model: 'text-davinci-003',
                 prompt: myMessage,
+                temperature: 0.2,
+                max_tokens: 200,
+                top_p: 1.0,
+                frequency_penalty: 0.2,
+                presence_penalty: 0.2,
+                stop: ["\n+"],
+            })
+            await bot.sendMessage(chatId, completion.data.choices[0].text);
+            
+            // console.log("result: ", result);          
+                }            
+        
+        
+        getapi(api_url)
+        // console.log("result",result);
+        // await bot.sendMessage(chatId, `Start `)
+
+
+
+
+            }
+//             else if (""){
+
+//                 const api_url = 
+//                 `https://sheetdb.io/api/v1/keqbu2v4inoqz/ `;
+//                 an= async function getapi(url) {
+              
+//              let response = await fetch(url);
+              
+//               var data = await response.json();
+// //  console.log("data",data);
+//             //   texts(data)
+//             let datas=JSON.stringify(data);
+//         //   text(datas)
+//     return datas    
+//     }
+//   abc=getapi(api_url)
+// abc()
+//   console.log();    
+//   await bot.sendMessage(chatId, abc()       )     }
+//             else if ( text.split(' ')[0]="start" ){
+//                 // Pass model settings to OpenAI
+
+//    const api_url = 
+//                         `https://sheetdb.io/api/v1/keqbu2v4inoqz/ `;
+                    
+//                   async function getapi(url) {
+                      
+//                      let response = await fetch(url);
+                      
+//                       var data = await response.json();
+//          console.log("data",data);
+//                     }
+//           getapi(api_url)
+
+//                 const completion = await openai.createCompletion({
+//                     model: 'text-davinci-003',
+//                     prompt: text,
+//                     temperature: 0.2,
+//                     max_tokens: 180,
+//                     top_p: 1.0,
+//                     frequency_penalty: 0.2,
+//                     presence_penalty: 0.2,
+//                     stop: ["\n+"],
+//                 })
+//                 // send the generated response to the chat
+//                 await bot.sendMessage(chatId, completion.data.choices[0].text);
+//                 console.log(completion.data);
+//         }
+            
+            
+            else if ( myMessage = text ){
+            // Pass model settings to OpenAI
+            const completion = await openai.createCompletion({
+                model: 'text-davinci-003',
+                prompt: text,
                 temperature: 0.2,
                 max_tokens: 180,
                 top_p: 1.0,
@@ -48,7 +159,9 @@ Upon receipt of the '/start' message, the bot is activated and sends  a message 
             // send the generated response to the chat
             await bot.sendMessage(chatId, completion.data.choices[0].text);
             console.log(completion.data);
-        })
+    }
+
+})
     // If there are errors, print them to the console
     } catch (error) {
         if (error.response) {
@@ -59,3 +172,7 @@ Upon receipt of the '/start' message, the bot is activated and sends  a message 
         }
     }
 }) ();
+
+
+
+
